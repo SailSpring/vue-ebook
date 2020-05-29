24ff214472a578884bf178afc6ba1a1fbd21654b
changfengpo <2590593050@qq.com>
2020-05-29 17:21:41 +0800

    修改生产环境的配置文件

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,15 @@
 // 传递的参数，第一个是全局的application对象 第二个是要模拟的接口的url 第三个是要传递什么样的数据
-function mock(app, url, data) {
-  app.get(url, (request, response) => {
-    response.json(data)
-  })
-}
+// function mock(app, url, data) {
+//   app.get(url, (request, response) => {
+//     response.json(data)
+//   })
+// }
 
 // 依次传入data
-const homeData = require('./src/mock/bookHome')
-const shelfData = require('./src/mock/bookShelf')
-const listData = require('./src/mock/bookList')
-const flatListData = require('./src/mock/bookFlatList')
+// const homeData = require('./src/mock/bookHome')
+// const shelfData = require('./src/mock/bookShelf')
+// const listData = require('./src/mock/bookList')
+// const flatListData = require('./src/mock/bookFlatList')
 
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production'
@@ -18,20 +18,20 @@ module.exports = {
   devServer: {
     public: '120.95.132.23:8080',
     hot: true,
-    disableHostCheck: true,
-    before (app) {
-      mock(app, '/book/home', homeData)
-      mock(app, '/book/shelf', shelfData)
-      mock(app, '/book/list', listData)
-      mock(app, '/book/flat-list', flatListData)
-    }
+    disableHostCheck: true
+    // before (app) {
+    //   mock(app, '/book/home', homeData)
+    //   mock(app, '/book/shelf', shelfData)
+    //   mock(app, '/book/list', listData)
+    //   mock(app, '/book/flat-list', flatListData)
+    // }
   },
   // 配置webpack,手动写一个属性 configureWebpack
   configureWebpack: {
     performance: {
       hints: 'warning',
-      maxAssetSize: 524288,
-      maxEntrypointSize: 524288
+      maxAssetSize: 524288 * 10,
+      maxEntrypointSize: 524288 * 10
     }
   }
 }
